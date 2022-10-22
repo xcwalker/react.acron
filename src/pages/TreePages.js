1aea307adddc296356f63dee3db977e4bfb16183
XCWalker | Xander Walker <xcwalker21@gmail.com>
2022-10-22 12:11:48 +0100

    More Initi

---
+++ src/pages/TreePages.js
@@ -0,0 +1,22 @@
+export function TreeSearch() {
+    // Search Existing Trees
+        // Using:
+            // User Displayname && Firstname && Lastname
+            // Tree Id
+            // Tree Title
+            // Tree Statement
+            // Tree Links
+    // Show if TreeID is unclimaed
+}
+
+export function TreeIndex() {
+    // if (!clamied)
+        // offer to claim tree
+    
+    // if (clamied)
+        // load tree
+}
+
+export function TreeEdit() {
+    // Edit an owned tree
+}
