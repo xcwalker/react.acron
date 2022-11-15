3ec86855559641f5b339a4e52a2903b4b70f72a4
XCWalker | Xander Walker <xcwalker21@gmail.com>
2022-11-15 21:33:12 +0000

    Fixed error in footer.

--- src/App.js
+++ src/App.js
@@ -113,8 +113,8 @@ export const network = "XCWALKER"
 export const release = "dev"
 export const url = application + "." + network + "." + release
 
-export const contactEmail = "contact@" + network + "." + release
-export const contactEmailMainDev = "xander@" + network + "." + release
+export const contactEmail = "contact@" + network.toLowerCase() + "." + release
+export const contactEmailMainDev = "xander@" + network.toLowerCase() + "." + release
 export const discordServer = "https://discord.gg/rjd7Spr"
 
 export const routeAbout = "about"
